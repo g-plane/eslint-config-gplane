ec117708b2c62cef5c00f54e33ae7c32e3ca02c6
Pig Fang <g-plane@hotmail.com>
2017-12-20 22:18:20 +0800

    feat(eslint-comments): eslint-comments

---
+++ eslint-comments.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'eslint-comments/disable-enable-pair': [2, { allowWholeFile: true }],
+    'eslint-comments/no-aggregating-enable': 2,
+    'eslint-comments/no-duplicate-disable': 2,
+    'eslint-comments/no-unused-disable': 2,
+    'eslint-comments/no-unused-enable': 2,
+  }
+}
