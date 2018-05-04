fbdeb87cb9d62e448b0293cf56054d4c0d00e9ca
Pig Fang <g-plane@hotmail.com>
2018-05-04 09:09:18 +0800

    breaking: change style: "space-before-function-paren"

--- tests/rules/base/space-before-function-paren.test.ts
+++ tests/rules/base/space-before-function-paren.test.ts
@@ -4,11 +4,11 @@ import options = require('../../../.eslintrc.json')
 const cli = new CLIEngine(options)
 
 test('incorrect', () => {
-  expect(cli.executeOnText('function fn() {}'))
+  expect(cli.executeOnText('function fn () {}'))
     .toHasLintingError('space-before-function-paren')
 })
 
 test('correct', () => {
-  expect(cli.executeOnText('function fn () {}'))
+  expect(cli.executeOnText('function fn() {}'))
     .not.toHasLintingError('space-before-function-paren')
 })
