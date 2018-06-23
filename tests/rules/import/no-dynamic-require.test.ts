15b12f0cfae897c260a6dc80653dd70018d9e836
Pig Fang <g-plane@hotmail.com>
2018-06-23 17:32:23 +0800

    feat(eslint-config): remove "eslint-plugin-import"

--- tests/rules/import/no-dynamic-require.test.ts
+++
@@ -1,9 +0,0 @@
-import { CLIEngine } from 'eslint'
-import options = require('../../..')
-
-const cli = new CLIEngine(options)
-
-test('incorrect', () => {
-  expect(cli.executeOnText('require(a)'))
-    .toHasLintingWarning('import/no-dynamic-require')
-})
