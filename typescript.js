57f7e401d2d6233a56e08ed139e43a9154ee18ac
Pig Fang <g-plane@hotmail.com>
2018-08-28 11:10:56 +0800

    feat(eslint-config): support typescript

---
+++ typescript.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'typescript/adjacent-overload-signatures': 2,
+    'typescript/member-delimiter-style': [2, { delimiter: 'none' }],
+    'typescript/no-angle-bracket-type-assertion': 2,
+    'typescript/no-array-constructor': 2,
+    'typescript/no-triple-slash-reference': 2,
+    'typescript/no-unused-vars': 2,
+    'typescript/type-annotation-spacing': 2,
+  }
+}
